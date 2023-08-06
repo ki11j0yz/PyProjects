486f065c895117546c0b00e3a14af451f6dab98f
James <jkprice02@gmail.com>
2023-08-05 20:43:34 -0500

    4th commit

--- wikipedia_scraper.py/scraper.py
+++ wikipedia_scraper.py/scraper.py
@@ -2,27 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import random
 
-response = requests.get(
-    url = "https://en.wikipedia.org/wiki/Web_scraping",
-)
-#print(response.status_code)
-soup = BeautifulSoup(response.content, 'html.parser')
+def scrapeWikiArticle(url):
+	response = requests.get(
+		url=url,
+	)
+	
+	soup = BeautifulSoup(response.content, 'html.parser')
 
-title = soup.find(id="firstHeading")
-print(title.string)
+	title = soup.find(id="firstHeading")
+	print(title.text)
 
-# Get all the links
-allLinks = soup.find(id="bodyContent").find_all("a")
-random.shuffle(allLinks)
-linkToScrape = 0
+	allLinks = soup.find(id="bodyContent").find_all("a")
+	random.shuffle(allLinks)
+	linkToScrape = 0
 
-for link in allLinks:
-    # We are only interested in other wiki articles
-    if link['href'].find("/wiki/") == -1:
-        continue
-    
-    #use this link to scrape
-    linkToScrape = link
-    break
+	for link in allLinks:
+		# We are only interested in other wiki articles
+		if link['href'].find("/wiki/") == -1: 
+			continue
 
-print(linkToScrape)
+		# Use this link to scrape
+		linkToScrape = link
+		break
+
+	scrapeWikiArticle("https://en.wikipedia.org" + linkToScrape['href'])
+
+scrapeWikiArticle("https://en.wikipedia.org/wiki/Web_scraping")
